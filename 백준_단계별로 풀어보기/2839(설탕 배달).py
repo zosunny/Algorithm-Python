ff0fba24ed03f3edf3dacab3d405d008c644430b
zomochii <zoheyy108@naver.com>
2022-07-01 00:34:15 +0900

    +주석

--- 백준_단계별로 풀어보기/2839(설탕 배달).py
+++ 백준_단계별로 풀어보기/2839(설탕 배달).py
@@ -2,11 +2,11 @@ n = int(input())
 cnt = 0
 
 while n >= 0:
-    if n % 5 == 0:
-        cnt += int(n // 5)
+    if n % 5 == 0:          # 5로 나누어 떨어지면
+        cnt += n // 5       # 몫을 더함
         print(cnt)
         break
-    n -= 3
-    cnt += 1
-else:
-    print(-1)
+    n -= 3                  # 5로 나누어 떨어지지 않으면 3을 빼고
+    cnt += 1                # + 1
+else:                       # while의 조건에 맞지 않으면
+    print(-1)               # (5로도 3으로도 안나눠 떨어지면) -1
