8b73f1ebe26ed9beb50375094ef585f52db20244
zomochii <zoheyy108@naver.com>
2022-07-02 16:18:49 +0900

    return True/False하고 if 함수(i): print(i) 처리

---
+++ 백준_단계별로 풀어보기/1929(소수 구하기).py
@@ -0,0 +1,15 @@
+def isPrime(n):
+    if n == 1:
+        return False
+    for j in range(2, int(n**0.5)+1):
+        if n % j == 0:
+            return False
+    return True             // n == 2 or 3 일때 소수지만 range를 만족하지는 않음
+
+m, n = map(int, input().split())
+
+for i in range(m, n+1):
+    if isPrime(i):
+        print(i)
+
+print(int(4**0.5)+1)
