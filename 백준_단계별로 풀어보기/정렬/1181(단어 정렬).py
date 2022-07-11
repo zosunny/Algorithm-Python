268226c95cbbed805768553d6a94943a116e8198
zomochii <zoheyy108@naver.com>
2022-07-11 21:08:41 +0900

    nums.sort() nums.sort(key=lambda i: len(i))     # sort(key = len) 랑 같음

--- 백준_단계별로 풀어보기/정렬/1181(단어 정렬).py
+++ 백준_단계별로 풀어보기/정렬/1181(단어 정렬).py
@@ -2,10 +2,9 @@ import sys
 input = sys.stdin.readline
 
 n = int(input())
-nums = set(input().strip() for _ in range(n))
-nums = list(nums)
-
+nums = set(input().strip() for _ in range(n))       # sys로 문자열입력시 strip()안하면
+nums = list(nums)                                   # 출력형식이 잘못되었습니다 뜸
 nums.sort()
-nums.sort(key=lambda i: len(i))
+nums.sort(key=lambda i: len(i))     # sort(key = len) 랑 같음
 for i in nums:
     print(i)
