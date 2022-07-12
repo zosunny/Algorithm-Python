edaaf1fcaf1bceb74d42e2aeb5d01e420771aaca
zomochii <zoheyy108@naver.com>
2022-07-13 00:31:58 +0900

    시간초과.

--- 백준_단계별로 풀어보기/정렬/18870(좌표 압축).py
+++ 백준_단계별로 풀어보기/정렬/18870(좌표 압축).py
@@ -1,12 +1,22 @@
 import sys
 input = sys.stdin.readline
 
-mark = []
 n = int(input())
+mark = [0] * n
 dots = list(map(int, input().split()))
 origin = dots[:]                        # [ 2 4 -10 4 -9 ]
-dots.sort(reverse=True)                 # [ -10 -9 2 4 4 ]
+dots.sort()                             # [ -10 -9 2 4 4 ]
+# print("origin: ", origin)
+# print("dots: ", dots)
+for i in range(1, n):
+    if dots[i] != dots[i-1]:
+        mark[i] += mark[i-1] + 1
+    else:
+        mark[i] = mark[i-1]
+# print("mark: ", mark)
 
-# for i in range(len(dots)-1):
-#     if dots[i] != dots[i+1]:
-#         mark[]
+for k in origin:
+    for l in range(n):
+        if k == dots[l]:
+            print(mark[l], end=" ")
+            break
