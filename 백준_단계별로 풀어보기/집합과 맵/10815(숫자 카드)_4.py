9b1299f4d9eb1684713521dedfc0309f2199d536
zomochii <zoheyy108@naver.com>
2022-07-14 00:36:14 +0900

    성공. set은 해시로 구현되어 있어, x in set 이 x in list보다 일반적으로 더 빨리 동작 함.

--- 백준_단계별로 풀어보기/집합과 맵/10815(숫자 카드)_4.py
+++ 백준_단계별로 풀어보기/집합과 맵/10815(숫자 카드)_4.py
@@ -11,3 +11,6 @@ for i in card2:
         print(1, end=" ")
     else:
         print(0, end=" ")
+
+# list에서 in 수행시 모든 요소를 처음부터 검사하면서 찾는 원소가 있는지 확인하지만,
+# set은 해시로 구현되어 있어 x in set 이 x in list보다 일반적으로 더 빨리 동작하게 된다.
