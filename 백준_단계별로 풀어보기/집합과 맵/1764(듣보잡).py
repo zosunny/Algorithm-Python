e943ae7cc2837496c3228ff713eae5e67357cb4d
zomochii <zoheyy108@naver.com>
2022-07-16 23:52:07 +0900

    성공. 집합과 리스트의 원소 수는 len 이용

--- 백준_단계별로 풀어보기/집합과 맵/1764(듣보잡).py
+++ 백준_단계별로 풀어보기/집합과 맵/1764(듣보잡).py
@@ -2,10 +2,10 @@ import sys
 input = sys.stdin.readline
 
 n, m = map(int, input().split())
-hear = set(input().rstrip() for _ in range(n))
-see = set(input().rstrip() for _ in range(m))
-both = list(hear & see)
-both.sort()
-print(len(both))
+hear = set(input().rstrip() for _ in range(n))      # 듣도 못한 사람
+see = set(input().rstrip() for _ in range(m))       # 보도 못한 사람
+both = list(hear & see)     # 듣도 보도 못한 사람
+both.sort()                 # 사전 순 정렬
+print(len(both))            # 인원 수
 for i in both:
     print(i)
