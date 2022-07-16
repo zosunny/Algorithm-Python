cd1829a16e1520c1d6608b223a2a60eeb37625e8
zomochii <zoheyy108@naver.com>
2022-07-16 23:42:43 +0900

    시간초과. 딕셔너리만 이용

--- 백준_단계별로 풀어보기/집합과 맵/10816(숫자 카드2).py
+++ 백준_단계별로 풀어보기/집합과 맵/10816(숫자 카드2).py
@@ -3,19 +3,18 @@ input = sys.stdin.readline
 
 n = int(input())
 sg = list(map(str, input().split()))
-sg2 = set(sg)
+sg2 = set(sg)          # 중복제거
 cnt = 0
 dic = {}
 for i in sg2:
-    cnt = sg.count(i)
-    dic[i] = cnt
+    cnt = sg.count(i)   # 상근이 카드 번호 마다 몇장이 있는지
+    dic[i] = cnt        # 딕셔너리에 저장(추가)
 
 m = int(input())
 nums = list(map(str, input().split()))
-print(nums)
 
-for j in nums:
-    if j in sg2:
-        print(dic[j], end=" ")
+for j in nums:                      # 입력받은 수가
+    if j in sg2:                    # 상근이 카드에 있으면
+        print(dic[j], end=" ")      # 딕셔너리에서 value값 출력
     else:
-        print(0, end=" ")
+        print(0, end=" ")           # 없으면 0 출력
