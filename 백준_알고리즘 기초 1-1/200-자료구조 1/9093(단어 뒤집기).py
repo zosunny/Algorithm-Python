9dc3023bf5ec7d2ba3370e72c94cfc9074bf8b72
zomochii <zoheyy108@naver.com>
2022-07-20 16:17:47 +0900

    성공. 뒤집은 단어 리스트 만들어 join

---
+++ 백준_알고리즘 기초 1-1/200-자료구조 1/9093(단어 뒤집기).py
@@ -0,0 +1,10 @@
+import sys
+input = sys.stdin.readline
+t = int(input())
+for _ in range(t):
+    s = list(input().rstrip().split())
+    r_word = []
+    for i in s:
+        r_word.append(i[::-1])
+    ans = " ".join(r_word)
+    print(ans)
