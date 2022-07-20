a7bf9c7c0e5e23676435e6531d18c0c913272280
zomochii <zoheyy108@naver.com>
2022-07-20 16:55:30 +0900

    성공. 람다 함수 사용

--- 백준_알고리즘 기초 1-1/200-자료구조 1/9093(단어 뒤집기)_2.py
+++ 백준_알고리즘 기초 1-1/200-자료구조 1/9093(단어 뒤집기)_2.py
@@ -3,4 +3,5 @@ input = sys.stdin.readline
 t = int(input())
 for _ in range(t):
     s = list(map(lambda x: x[::-1], input().split()))
-    print(" ".join(s))
+    print(" ".join(s))      # 리스트를 " ".join 이용해
+                            # 공백을 기준으로 조합해 출력
