7c242717b73449f25294fae1730b25e842ea9088
zomochii <zoheyy108@naver.com>
2022-07-21 18:45:43 +0900

    성공. 뒤집은 새 리스트 만들어 join

--- 백준_알고리즘 기초 1-1/200-자료구조 1/9093(단어 뒤집기).py
+++ 백준_알고리즘 기초 1-1/200-자료구조 1/9093(단어 뒤집기).py
@@ -5,6 +5,6 @@ for _ in range(t):
     s = list(input().rstrip().split())
     r_word = []
     for i in s:
-        r_word.append(i[::-1])
+        r_word.append(i[::-1])  # 뒤집기
     ans = " ".join(r_word)
     print(ans)
