7510223045cc8e4fae774491ef2b93a17bb3957c
zomochii <zoheyy108@naver.com>
2022-07-21 22:05:22 +0900

    성공!!. 주석 첨가

--- 백준_알고리즘 기초 1-1/200-자료구조 1/9012(괄호)_2.py
+++ 백준_알고리즘 기초 1-1/200-자료구조 1/9012(괄호)_2.py
@@ -5,11 +5,11 @@ t = int(input())
 for _ in range(t):
     data = input().rstrip()
     while(True):
-        if "()" in data:
-            data = data.replace("()", "")
-        else:
-            break
-    if len(data) == 0:
-        print("YES")
+        if "()" in data:                    # 문자열에 () 가 있으면
+            data = data.replace("()", "")   # 문자열에서 () 를 제거한다
+        else:                               # 더이상 ()가 없으면
+            break                           # 탈출
+    if len(data) == 0:          # ()를 제거한 문자열에 아무것도 없으면
+        print("YES")            # VPS 임
     else:
         print("NO")
