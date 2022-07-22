9d0a1f0e1dfb7d763d4c39ea2a00ef50733c0462
zomochii <zoheyy108@naver.com>
2022-07-22 16:59:25 +0900

    틀림..테스트케이스도 맞지 않음

---
+++ 백준_알고리즘 기초 1-1/200-자료구조 1/1874(스택 수열).py
@@ -0,0 +1,26 @@
+import sys
+input = sys.stdin.readline
+
+def push(x):
+    stack.append(x)
+    print("+")
+    print(stack)
+def pop():
+    stack.pop()
+    print("-")
+    print(stack)
+
+n = int(input())
+seq = []
+stack = []
+flag = 0
+for _ in range(n):
+    seq.append(int(input()))
+
+for i in range(1,n+2):
+    if seq[flag] in stack:
+        pop()
+        flag += 1
+    else:
+        push(i)
+        i += 1
