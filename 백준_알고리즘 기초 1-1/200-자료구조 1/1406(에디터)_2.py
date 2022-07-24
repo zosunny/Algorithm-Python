eaf3f69a055f97d270a654b917c018b51f6d457f
zomochii <zoheyy108@naver.com>
2022-07-25 00:31:50 +0900

    2개의 좌우 스택으로 커서 위치 표현

--- 백준_알고리즘 기초 1-1/200-자료구조 1/1406(에디터)_2.py
+++ 백준_알고리즘 기초 1-1/200-자료구조 1/1406(에디터)_2.py
@@ -1,19 +1,19 @@
 import sys
 
-stack_l = list(input())
-stack_r = []
+stack_l = list(input()) # 커서 기준 왼쪽
+stack_r = []            # 커서 기준 오른쪽
 n = int(input())
 
 for i in range(n):
-    cmd = sys.stdin.readline().split()
+    order_list = sys.stdin.readline().split()
+    order = order_list[0]
+    if order == "L" and stack_l:        # L이고 왼쪽에 값이 있으면(True면)
+        stack_r.append(stack_l.pop())   # 왼쪽 값 pop해 오른쪽에 넣음
+    elif order == "D" and stack_r:      # D이고 오른쪽에 값이 있으면(True면)
+        stack_l.append(stack_r.pop())   # 오른쪽 값 pop해 왼쪽에 넣음
+    elif order == "B" and stack_l:      # B이고 왼쪽에 값이 있으면(True면)
+        stack_l.pop()                   # 왼쪽 값 삭제
+    elif order == "P":                  # P면
+        stack_l.append(order_list[1])   # 왼쪽에 입력 받은 값 추가
 
-    if cmd[0] == "L" and stack_l:
-        stack_r.append(stack_l.pop())
-    elif cmd[0] == "D" and stack_r:
-        stack_l.append(stack_r.pop())
-    elif cmd[0] == "B" and stack_l:
-        stack_l.pop()
-    elif cmd[0] == "P":
-        stack_l.append(cmd[1])
-
-print("".join(stack_l + list(reversed(stack_r))))
+print("".join(stack_l + list(reversed(stack_r))))   # 오른쪽 리스트는 역순해서 붙여줘야 함
