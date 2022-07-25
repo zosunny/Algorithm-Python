94460bfec8fc23e0b0f3e0d847f170cd17e5240d
zomochii <zoheyy108@naver.com>
2022-07-25 23:54:49 +0900

    정답!! 인덱스를 정의하는 로직

--- 백준_알고리즘 기초 1-1/200-자료구조 1/1158(요세푸스 문제).py
+++ 백준_알고리즘 기초 1-1/200-자료구조 1/1158(요세푸스 문제).py
@@ -8,9 +8,9 @@ ans = []
 num = 0
 
 for _ in range(n):
-    num += k-1
-    if num >= len(plist):
-        num %= len(plist)
-    ans.append(str(plist.pop(num)))
+    num += k-1                          # 인덱스 0부터 시작이니까 -1해서 누적
+    if num >= len(plist):               # 한 바퀴 넘어가면
+        num %= len(plist)               # 찾는 인덱스 재정의
+    ans.append(str(plist.pop(num)))     # plist에서 인덱스가 num인 값 pop
 
 print("<",", ".join(ans), ">", sep="")
