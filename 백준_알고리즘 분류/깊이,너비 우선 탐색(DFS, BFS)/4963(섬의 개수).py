734621e0712402a9a163247af843896bc01504f9
zomochii <zoheyy108@naver.com>
2022-08-03 18:04:18 +0900

    w,h 는 행,렬이 아닌 렬,행 이다. 따라서 반대로 입력 (대신 그래프가 90도 뒤집어 짐)

--- 백준_알고리즘 분류/깊이,너비 우선 탐색(DFS, BFS)/4963(섬의 개수).py
+++ 백준_알고리즘 분류/깊이,너비 우선 탐색(DFS, BFS)/4963(섬의 개수).py
@@ -17,9 +17,10 @@ def dfs(x, y):
         dfs(x-1, y-1)
         return True
     return False
-
+# 이문제는 어차피 섬의 수만 출력하면 되니까 행렬을 바꿔도 상관없으나
+# 이렇게 하지는 말자..
 while True:
-    h, w= map(int, input().split())
+    h, w = map(int, input().split())
     if w == 0 and h == 0:
         break
     # 2차원 리스트의 맵 정보 입력
