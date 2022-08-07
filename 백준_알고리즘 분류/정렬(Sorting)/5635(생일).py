c38b0d3f0d36221db3de6965678ffde30575c316
zomochii <zoheyy108@naver.com>
2022-08-07 22:50:20 +0900

    정렬 - 람다함수 정렬 이용

--- 백준_알고리즘 분류/정렬(Sorting)/5635(생일).py
+++ 백준_알고리즘 분류/정렬(Sorting)/5635(생일).py
@@ -5,7 +5,7 @@ n = int(input())
 st = []
 for _ in range(n):
     n, d, m, y = input().split()
-    st.append([n, int(d), int(m), int(y)])
-st.sort(key=lambda x: (x[3], x[2], x[1]))
-print(st[-1][0])
-print(st[0][0])
+    st.append([n, int(d), int(m), int(y)])  # 2차원으로 리스트 추가
+st.sort(key=lambda x: (x[3], x[2], x[1]))   # 년,월,일 순으로 정렬
+print(st[-1][0])    # 가장 나이 적은 사람
+print(st[0][0])     # 가장 나이 많은 사람
