01f631642383a5c2d4f98a23b5f0dd494b0607ef
zomochii <zoheyy108@naver.com>
2022-08-12 20:12:59 +0900

    그리디 - 힙큐 주석추가

--- 백준_알고리즘 분류/그리디 알고리즘(Greedy)/1374(강의실).py
+++ 백준_알고리즘 분류/그리디 알고리즘(Greedy)/1374(강의실).py
@@ -4,12 +4,16 @@ input = sys.stdin.readline
 
 N = int(input())
 l = [list(map(int, input().split())) for _ in range(N)]
-l.sort(key = lambda x: x[1])
+#print(l)
+l.sort(key=lambda x: x[1])
+#print(l)
 mh = []
 cnt = 0
-for x in l:
-    while mh and mh[0] <= x[1]:
-        heapq.heappop(mh)
-    heapq.heappush(mh, x[2])
+for i in l:
+    while mh and mh[0] <= i[1]: # 가장 일찍 끝나는 시간보다 시작 시간이 크면
+        heapq.heappop(mh)       # mh에서 가장 작은 원소 pop & return
+    heapq.heappush(mh, i[2])    # mh에 i[2]=끝나는 시간을 추가
+    #print(mh)
     cnt = max(cnt, len(mh))
+
 print(cnt)
