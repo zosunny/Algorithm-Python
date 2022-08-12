27c66476c1b93d7758e703aff077cfc82045b610
zomochii <zoheyy108@naver.com>
2022-08-12 23:50:45 +0900

    구현 - 다시

---
+++ 백준_알고리즘 분류/구현(Implementation)/14503(로봇 청소기).py
@@ -0,0 +1,37 @@
+import sys
+input = sys.stdin.readline
+
+n, m = map(int, input().split())
+graph = []
+visit = [[0] * m for _ in range(n)]
+r, c, d = map(int, input().split())
+
+dx = [-1, 0, 1, 0]
+dy = [0, 1, 0, -1]
+
+for _ in range(n):
+    graph.append(list(map(int, input().split())))
+
+visit[r][c] = 1
+cnt = 1
+
+while 1:
+    flag = 0
+    for _ in range(4):
+        nx = r + dx[(d+3) % 4]
+        ny = c + dy[(d+3) % 4]
+        d = (d+3) % 4
+        if 0 <= nx < n and 0 <= ny < m and graph[nx][ny] == 0:
+            if visit[nx][ny] == 0:
+                visit[nx][ny] = 1
+                cnt += 1
+                r = nx
+                c = ny
+                flag = 1
+                break
+    if flag == 0:
+        if graph[r-dx[d]][c-dy[d]] == 1:
+            print(cnt)
+            break
+        else:
+            r, c = r-dx[d], c-dy[d]
