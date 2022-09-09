438434f45e47077747f39677e86d7a9b5924d168
zomochii <zoheyy108@naver.com>
2022-09-09 21:14:15 +0900

    구현-que 주석추가

--- 백준_알고리즘 분류/구현(Implementation)/1966(프린터 큐).py
+++ 백준_알고리즘 분류/구현(Implementation)/1966(프린터 큐).py
@@ -5,15 +5,15 @@ input = sys.stdin.readline
 tc = int(input())
 for _ in range(tc):
     n, m = map(int, input().split())
-    que = deque(list(map(int, input().split())))
-    idx = deque(list(range(n)))
+    que = deque(list(map(int, input().split())))    # 중요도
+    idx = deque(list(range(n)))                     # 인덱스
     cnt = 0
     while que:
-        if que[0] == max(que):
-            cnt += 1
-            que.popleft()
-            if idx.popleft() == m:
-                print(cnt)
+        if que[0] == max(que):      # 첫번째 요소가 최대값이면
+            cnt += 1                # 카운트
+            que.popleft()           # pop
+            if idx.popleft() == m:  # 인덱스가 m과 같을 때
+                print(cnt)          # 카운트 값 출력
         else:
-            que.append(que.popleft())
+            que.append(que.popleft())   # 왼쪽 값 pop해 append
             idx.append(idx.popleft())
