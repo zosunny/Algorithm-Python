b6946c885af33ec76d78c159a5f433b058890aab
zomochii <zoheyy108@naver.com>
2022-09-11 00:21:30 +0900

    구현-deque.rotate 주석추가

--- 백준_알고리즘 분류/구현(Implementation)/11866(요세푸스 문제 0).py
+++ 백준_알고리즘 분류/구현(Implementation)/11866(요세푸스 문제 0).py
@@ -1,10 +1,11 @@
 import sys
 from collections import deque
 input = sys.stdin.readline
-n, k = map(int,input().split())
-circle = deque([x for x in range(1,n+1)])
+n, k = map(int, input().split())
+circle = deque([i for i in range(1, n+1)])
 removed = []
 while circle:
-    circle.rotate(-(k-1))
-    removed.append(circle.popleft())
-print(f'<{", ".join(map(str,removed))}>')
+    circle.rotate(-(k-1))   # k-1만큼 왼쪽으로 리스트 이동
+    removed.append(circle.popleft()) # k번째였던 수 제거
+
+print(f'<{", ".join(map(str, removed))}>')  # f-string 문자열 포매팅 {}안의 값만 바뀌고 나머지는 일정
