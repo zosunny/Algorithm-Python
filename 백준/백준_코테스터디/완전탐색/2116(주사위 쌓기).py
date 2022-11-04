bad92c933d6384ef6c59ffb95545128b49ceb5e5
zosunny <zoheyy108@naver.com>
2022-11-04 14:45:05 +0900

    2022.11.4-완전탐색(brute force)

---
+++ 백준/백준_코테스터디/완전탐색/2116(주사위 쌓기).py
@@ -0,0 +1,26 @@
+n = int(input())
+dice = []
+for _ in range(n):
+    dice.append(list(map(int, input().split())))
+pair = {0: 5, 1: 3, 2: 4, 3: 1, 4: 2, 5: 0}
+
+maxnum = 0                          # 최댓값
+
+for i in range(6):                  # 첫 번째 주사위 밑면 기준으로 반복
+    max_side = []
+    tmp = [1, 2, 3, 4, 5, 6]
+    tmp.remove(dice[0][i])          # 첫 번째 주사위의 bottom 삭제
+    next = dice[0][pair[i]]         # 첫 번째 주사위의 up 이자 다음 주사위의 bottom
+    tmp.remove(next)                # 첫 번째 주사위의 up 삭제
+    max_side.append(max(tmp))       # 옆면중 최댓값
+    for j in range(1, n):           # 2~마지막 주사위까지 반복
+        tmp = [1, 2, 3, 4, 5, 6]
+        tmp.remove(next)
+        next = dice[j][pair[dice[j].index(next)]]       # 아랫면 값의 인덱스로 윗면값 구함
+        tmp.remove(next)                                # 윗면 삭제
+        max_side.append(max(tmp))                       # 옆면중 최댓값
+    max_side = sum(max_side)                            # 각 주사위별 옆면의 최대값 합
+    if maxnum < max_side:
+        maxnum = max_side
+
+print(maxnum)
