df1c769888ae5769c2a596e82a323c88bca320cd
zosunny <zoheyy108@naver.com>
2022-11-13 01:36:35 +0900

    2022.11.12-성공

--- SWEA/d2/1859(백만 장자 프로젝트).py
+++ SWEA/d2/1859(백만 장자 프로젝트).py
@@ -1,23 +1,15 @@
 import sys
 input = sys.stdin.readline
 
-def solv(x):
-    ans = 0
-    buy = []
-    while True:
-        if len(price) == 0:
-            print("#", x, " ", ans, sep="")
-            break
-        else:
-            if price[0] < max(price):
-                buy.append(price.pop(0))
-            elif price[0] == max(price):
-                ans += price.pop(0) * len(buy) - sum(buy)
-                buy = []
-
 T = int(input())
 
 for x in range(T):
     N = int(input())
     price = list(map(int, input().split()))
-    solv(x+1)
+    ans = 0
+    maxP = price[-1]
+    for i in price[::-1]:   # 뒤에서부터 돌면서
+        if i > maxP:        # maxP에 최댓값을 넣으며
+            maxP = i
+        ans += maxP - i     # 최댓값보다 작으면 (현재값과)그 차이를 누적
+    print("#", x+1, " ", ans, sep="")
