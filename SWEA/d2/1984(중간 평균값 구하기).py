6dc3b8d9c14afb707f011228e6f63c57df6a2b4f
zosunny <zoheyy108@naver.com>
2022-11-14 19:05:28 +0900

    2022.11.14-pass-sort는 따로 해주자, 범위조심!, 반올림: round(값, 표현자릿수)

--- SWEA/d2/1984(중간 평균값 구하기).py
+++ SWEA/d2/1984(중간 평균값 구하기).py
@@ -6,5 +6,5 @@ for t in range(T):
     sum = 0
     for i in nums[1:9]:
         sum += i
-    ans = round(sum / (len(nums)-2))
+    ans = round(sum / (len(nums)-2))    # 반올림: round(값, 표현자릿수)
     print("#", t + 1, " ", ans, sep="")
