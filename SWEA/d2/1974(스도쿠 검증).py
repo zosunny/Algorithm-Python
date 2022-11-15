c225546e85b21599210a977c8481dbc3ab92fdf7
zosunny <zoheyy108@naver.com>
2022-11-15 23:59:29 +0900

    2022.11.15-pass-가로세로for문 따로 구현!

--- SWEA/d2/1974(스도쿠 검증).py
+++ SWEA/d2/1974(스도쿠 검증).py
@@ -23,22 +23,24 @@ for t in range(T):
             flag = 0
             tmp = set()
             break
-        # 사각형 탐색
-        # for j in range(9):
-        #     if i % 3 == 0 and j % 3 == 0:
-        #         for x in range(3):
-        #             for y in range(3):
-        #                 tmp.add(sudo[i+x][j+y])
-        #         if len(tmp) != 9:
-        #             flag = 0
-        #             break
-    for x in range(0, 9, 3):
-        for y in range(0, 9, 3):
+    # 사각형 탐색
+    for i in range(9):
+        for j in range(9):
             tmp = set()
-            for z in range(3):
-                for w in range(3):
-                    tmp.add(sudo[x+z][y+w])
-            if len(tmp) != 9:
-                flag = 0
-                break
+            if i % 3 == 0 and j % 3 == 0:
+                for x in range(3):
+                    for y in range(3):
+                        tmp.add(sudo[i+x][j+y])
+                if len(tmp) != 9:
+                    flag = 0
+                    break
+    # for x in range(0, 9, 3):
+    #     for y in range(0, 9, 3):
+    #         tmp = set()
+    #         for z in range(3):
+    #             for w in range(3):
+    #                 tmp.add(sudo[x+z][y+w])
+    #         if len(tmp) != 9:
+    #             flag = 0
+    #             break
     print("#", t+1, " ", flag, sep="")
