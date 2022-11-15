b0e5a95d4b3e918aefcfeff6f9e823d10eb5a2be
zosunny <zoheyy108@naver.com>
2022-11-15 23:45:00 +0900

    2022.11.15-fail(1차)

---
+++ SWEA/d2/1974(스도쿠 검증).py
@@ -0,0 +1,32 @@
+T = int(input())
+
+for t in range(T):
+    sudo = [list(map(int, input().split())) for _ in range(9)]
+
+    flag = 1
+    for i in range(9):
+        tmp = set()
+        # 가로 탐색
+        for j in range(9):
+            tmp.add(sudo[i][j])
+        if len(tmp) != 9:
+            flag = 0
+            tmp = set()
+            break
+        # 세로 탐색
+        for j in range(9):
+            tmp.add(sudo[j][i])
+        if len(tmp) != 9:
+            flag = 0
+            tmp = set()
+            break
+        # 사각형 탐색
+        for j in range(9):
+            if i % 3 == 0 and j % 3 == 0:
+                for x in range(3):
+                    for y in range(3):
+                        tmp.add(sudo[i+x][j+y])
+                if len(tmp) != 9:
+                    flag = 0
+                    break
+    print("#", t+1, " ", flag, sep="")
