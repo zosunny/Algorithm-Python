525a8363452bf83f77d9c4787f78bf1d2f65f854
zosunny <zoheyy108@naver.com>
2022-11-17 14:53:52 +0900

    2022.11.17-fail

--- 백준/백준_코테스터디/그리디/1107(리모컨).py
+++ 백준/백준_코테스터디/그리디/1107(리모컨).py
@@ -3,4 +3,28 @@ input = sys.stdin.readline
 
 N = int(input())
 M = int(input())
-broken = [map(int, input().split()) for _ in range(N)]
+if M == 0:
+    print(len(str(N)))
+else:
+    broken = list(map(int, input().split()))
+
+    if N == 100:
+        print(0)
+    else:
+        ltsN = [int(x) for x in str(N)]
+        newN = []
+        for i in ltsN:
+            while True:
+                if i in broken:
+                    if i == 1:
+                        i -= 1
+                    else:
+                        i += 1
+                else:
+                    newN.append(i)
+                    break
+        print(newN)
+        print(ltsN)
+        ans = abs(int(''.join(map(str, newN))) - int(''.join(map(str, ltsN)))) + len(ltsN)
+
+        print(ans)
